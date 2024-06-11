e0ea0834b0fb367ba7c8937614b1b88521affadb
mingmoth <mingmoth@gmail.com>
2024-06-11 13:27:51 +0800

    feat: add Block component

---
+++ src/components/Block.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Block.css';
+
+export default function Block({ block, disabled, onReveal, onFlag }) {
+
+  const blockDisplay = () => {
+    if (block.flagged) return 'F';
+    if (block.revealed) {
+      return block.mines ? '@' : block.adjacent || ''
+    };
+    return '';
+  };
+
+  const handleClick = () => {
+    if (block.revealed || block.flagged) return
+    onReveal(block.x, block.y);
+  };
+
+  const handleRightClick = (e) => {
+    e.preventDefault();
+
+    if (block.revealed) return
+    onFlag(block.x, block.y);
+  };
+
+  return (
+    <div
+      className={[
+        'block',
+        disabled && 'block-disabled',
+        block.revealed ? 'block-revealed' : 'block-hidden',
+        block.mines && block.revealed && 'block-mine',
+      ].filter(Boolean).join(' ')}
+      onClick={handleClick}
+      onContextMenu={handleRightClick}
+    >
+      {blockDisplay()}
+    </div>
+  );
+}
